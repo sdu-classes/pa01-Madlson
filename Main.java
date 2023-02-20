c397bd6372347011cdbafab52873afb114c49b8a
Madlson <120780771+Madlson@users.noreply.github.com>
2023-02-20 18:50:01 +0600

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,21 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        Circle c1 = new Circle();
-        System.out.println("c1 radius: " + c1.getRadius());
-        System.out.println("c1 area: " + c1.getArea());
-        System.out.println("c1 circumference: " + c1.getCircumference());
-        System.out.println("c1 toString: " + c1.toString());
+        Ball ball = new Ball(50, 50, 10, 5, 2);
+        System.out.println(ball);
 
-        Circle c2 = new Circle(2.5);
-        System.out.println("c2 radius: " + c2.getRadius());
-        System.out.println("c2 area: " + c2.getArea());
-        System.out.println("c2 circumference: " + c2.getCircumference());
-        System.out.println("c2 toString: " + c2.toString());
+        ball.move();
+        System.out.println(ball);
 
-        c2.setRadius(3.0);
-        System.out.println("c2 radius (after setRadius): " + c2.getRadius());
-        System.out.println("c2 area (after setRadius): " + c2.getArea());
-        System.out.println("c2 circumference (after setRadius): " + c2.getCircumference());
-        System.out.println("c2 toString (after setRadius): " + c2.toString());
+        ball.reflectHorizontal();
+        System.out.println(ball);
+
+        ball.move();
+        System.out.println(ball);
+
+        ball.reflectVertical();
+        System.out.println(ball);
+
+        ball.move();
+        System.out.println(ball);
     }
 }
