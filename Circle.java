6963a38b6f7e71d60bb65679d532575502dc1b22
Madlson <120780771+Madlson@users.noreply.github.com>
2023-02-20 17:29:41 +0600

    ProblemA
    ID: 210103449
    Name: Madi Meirambay

--- Circle.java
+++ Circle.java
@@ -1,30 +1,32 @@
-public class Circle{
+public class Circle {
     private double radius = 1.0;
 
-    public Circle(){}
-    public Circle(double radius){
+    public Circle() {
+    }
+
+    public Circle(double radius) {
         this.radius = radius;
     }
 
-    public double getRadius(){
+    public double getRadius() {
         return radius;
     }
 
-    public void setRadius(double newRadius){
+    public void setRadius(double radius) {
         this.radius = radius;
     }
-    public double getArea(){
-        return Math.PI*Math.pow(radius,2.0);
+
+    public double getArea() {
+        return Math.PI * radius * radius;
     }
-    public double getCicumference(){
-        return Math.PI*radius*2;
+
+    public double getCircumference() {
+        return 2 * Math.PI * radius;
     }
+
     @Override
-    public String toString(){
-        return "Circle radius = " + radius +
-                "\nArea = " + getArea() +
-                "\nCircumference = " + getCicumference();
-
+    public String toString() {
+        return "Circle[radius=" + radius + "]";
     }
-
 }
+
